75b681d53c6eaac2bed864cb101a1ed897a80ff4
MoYaXin <1413378838@qq.com>
2023-06-12 17:28:13 +0800

    添加http

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,19 @@ import {VantResolver} from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-		port: 3000,
-        host: "0.0.0.0"
+      port: 3000,
+      host: "0.0.0.0"
     },
-    plugins: [vue(),
-        Components({
-            resolvers: [VantResolver()],
-        }),
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [VantResolver()],
+      }),
     ],
-})
+    proxy: {
+      '/api': {
+        target: 'http://101.34.244.76:9099',
+        changeOrigin: true
+      }
+    }
+  });
